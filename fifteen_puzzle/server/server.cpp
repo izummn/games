9465e0a74ae2adfdbdb841a705c72349258b0292
izummn <maratik1993@mail.ru>
2015-12-08 02:30:03 +0300

    add_exit_mode
    Changes:
    1) add exit mode, enter 0 to exit
    2) add trace
    3) use some stl functions instead boost

--- fifteen_puzzle/server/server.cpp
+++ fifteen_puzzle/server/server.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/enable_shared_from_this.hpp>
 
 namespace ba = boost::asio;
 const int max = 512;
 ba::io_service service;
 
-class talk_to_client : public boost::enable_shared_from_this<talk_to_client>, boost::noncopyable
+class talk_to_client : public std::enable_shared_from_this<talk_to_client>, boost::noncopyable
 {
 
 public:
 	typedef boost::system::error_code error_code;
-	typedef boost::shared_ptr<talk_to_client> ptr;
+	typedef std::shared_ptr<talk_to_client> ptr;
 
 	void start()
 	{
@@ -31,7 +31,7 @@ public:
 	void stop()
 	{
 		if (!started) return;
-		started = false;
+		started = false;	
 		sock.close();
 	}
 
@@ -49,20 +49,31 @@ private:
 
 	void on_read(const error_code & err, size_t bytes)
 	{
+		std::cout << " Server is: on_read " << std::endl;
+
+		if (std::find(read_buffer, read_buffer + bytes, '0') < read_buffer + bytes)
+		{
+			std::cout << " Server is: exit " << std::endl;
+			stop();
+		}
+		else
+		{
 			std::string msg(read_buffer, bytes);
 			// send message back
 			do_write(" Response from server: " + msg + "\n");
+		}
+		
 	}
 
 	void on_write(const error_code & err, size_t bytes)
-	{
-		
+	{	
+		std::cout << " Server is: on_write " << std::endl;
 		do_read();
 	}
 
 	void do_read()
 	{
-		
+		std::cout << " Server is: do_read " << std::endl;
 		ba::async_read(sock, ba::buffer(read_buffer),
 			boost::bind(&self_type::read_complete, shared_from_this(), _1, _2),
 			boost::bind(&self_type::on_read, shared_from_this(), _1, _2));
@@ -70,14 +81,17 @@ private:
 
 	void do_write(const std::string & msg)
 	{
-		std::copy(msg.begin(), msg.end(), write_buffer);
+		std::cout << " Server is: do_write " << std::endl;
+		std::copy(msg.begin(), msg.end(), write_buffer);	
 		sock.async_write_some(ba::buffer(write_buffer, msg.size()),
 			boost::bind(&self_type::on_write, shared_from_this(), _1, _2));
 	}
 
 	size_t read_complete(const boost::system::error_code & err, size_t bytes)
 	{
+		std::cout << " Server is: read_complete " << std::endl;
 		if (err) return 0;
+
 		bool found = std::find(read_buffer, read_buffer + bytes, '\n') < read_buffer + bytes;
 		// we read one-by-one until we get to enter, no buffering
 		return found ? 0 : 1;
@@ -92,11 +106,15 @@ void handle_accept(talk_to_client::ptr client, const boost::system::error_code &
 	client->start();
 	talk_to_client::ptr new_client = talk_to_client::get_new_client();
 	acceptor.async_accept(new_client->get_sock(), boost::bind(handle_accept, new_client, _1));
+	
 }
 
 int main()
 {
 	talk_to_client::ptr client = talk_to_client::get_new_client();
+	auto endpoint = acceptor.local_endpoint();
+	std::cout << " Port is: " << endpoint.port() << std::endl;
 	acceptor.async_accept(client->get_sock(), boost::bind(handle_accept, client, _1));
 	service.run();
+	
 }
